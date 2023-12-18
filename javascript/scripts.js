9b9b00728193c524c0ecb7523d4dd6770833eacc
danieleangeletti <angelettidaniele@gmail.com>
2023-12-18 14:44:16 +0100

    milestone 0 con function

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -9,49 +9,29 @@ const src_img_array = [
     "./img/barbara-ramos-graphic-designer.jpg"
 ]
 
-const wayne_barnett = {
-    first_name: "Wayne",
-    last_name: "Barnett",
-    role: "Founder & CEO",
-    image: "./img/wayne-barnett-founder-ceo.jpg"
-}
-
-const angela_caroll = {
-    first_name: "Angela",
-    last_name: "Caroll",
-    role: "Chief Editor",
-    image: "./img/angela-caroll-chief-editor.jpg"
-}
-
-const walter_gordon = {
-    first_name: "Walter",
-    last_name: "Gordon",
-    role: "Office Manager",
-    image: "./img/walter-gordon-office-manager.jpg"
-}
-
-const angela_lopez = {
-    first_name: "Angela",
-    last_name: "Lopez",
-    role: "Social Media Manager",
-    image: "./img/angela-lopez-social-media-manager.jpg"
-}
-
-const scott_estrada = {
-    first_name: "Scott",
-    last_name: "Estrada",
-    role: "Developer",
-    image: "./img/scott-estrada-developer.jpg"
-}
-
-const barbara_ramos = {
-    first_name: "Barbara",
-    last_name: "Ramos",
-    role: "Graphic Designer",
-    image: "./img/barbara-ramos-graphic-designer.jpg"
-}
+const wayne_barnett = generate_character("Wayne", "Barnett", "Founder & CEO", "'./img/wayne-barnett-founder-ceo.jpg'");
+const angela_caroll = generate_character("Angela", "Caroll", "Chief Editor", "'./img/angela-caroll-chief-editor.jpg'");
+const walter_gordon = generate_character("Walter", "Gordon", "Office Manager", "'./img/walter-gordon-office-manager.jpg'");
+const angela_lopez = generate_character("Angela", "Lopez", "Social Media Manager", "'./img/angela-lopez-social-media-manager.jpg'");
+const scott_estrada = generate_character("Scott", "Estrada", "Developer", "'./img/scott-estrada-developer.jpg'");
+const barbara_ramos = generate_character("Barbara", "Ramos", "Graphic Designer", "'./img/barbara-ramos-graphic-designer.jpg'");
 
 our_team_array.push(wayne_barnett, angela_caroll, walter_gordon, angela_lopez, scott_estrada, barbara_ramos);
 
-console.log(our_team_array)
+console.log(our_team_array);
+
+// FUNCTION
+
+function generate_character(first_name, last_name, role, image) {
+    
+    const new_character = {
+        first_name: first_name,
+        last_name: last_name,
+        role: role,
+        image: image
+    };
+
+    return new_character;
+
+}
 
