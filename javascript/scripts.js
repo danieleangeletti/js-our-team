d82f4870f56b2256b94480a36a8cf4fb544f17a7
danieleangeletti <angelettidaniele@gmail.com>
2023-12-18 16:11:24 +0100

    src img migliorati

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,23 +1,13 @@
-const my_container = document.getElementById("my-container");
 const my_row = document.getElementById("my-row");
 
 const our_team_array = [];
 
-const src_img_array = [
-    "./img/wayne-barnett-founder-ceo.jpg",
-    "./img/angela-caroll-chief-editor.jpg",
-    "./img/walter-gordon-office-manager.jpg",
-    "./img/angela-lopez-social-media-manager.jpg",
-    "./img/scott-estrada-developer.jpg",
-    "./img/barbara-ramos-graphic-designer.jpg"
-]
-
-const wayne_barnett = generate_character("Wayne", "Barnett", "Founder & CEO", "./img/wayne-barnett-founder-ceo.jpg");
-const angela_caroll = generate_character("Angela", "Caroll", "Chief Editor", "./img/angela-caroll-chief-editor.jpg");
-const walter_gordon = generate_character("Walter", "Gordon", "Office Manager", "./img/walter-gordon-office-manager.jpg");
-const angela_lopez = generate_character("Angela", "Lopez", "Social Media Manager", "./img/angela-lopez-social-media-manager.jpg");
-const scott_estrada = generate_character("Scott", "Estrada", "Developer", "./img/scott-estrada-developer.jpg");
-const barbara_ramos = generate_character("Barbara", "Ramos", "Graphic Designer", "./img/barbara-ramos-graphic-designer.jpg");
+const wayne_barnett = generate_character("Wayne", "Barnett", "Founder & CEO", "wayne-barnett-founder-ceo.jpg");
+const angela_caroll = generate_character("Angela", "Caroll", "Chief Editor", "angela-caroll-chief-editor.jpg");
+const walter_gordon = generate_character("Walter", "Gordon", "Office Manager", "walter-gordon-office-manager.jpg");
+const angela_lopez = generate_character("Angela", "Lopez", "Social Media Manager", "angela-lopez-social-media-manager.jpg");
+const scott_estrada = generate_character("Scott", "Estrada", "Developer", "scott-estrada-developer.jpg");
+const barbara_ramos = generate_character("Barbara", "Ramos", "Graphic Designer", "barbara-ramos-graphic-designer.jpg");
 
 our_team_array.push(wayne_barnett, angela_caroll, walter_gordon, angela_lopez, scott_estrada, barbara_ramos);
 
@@ -32,7 +22,7 @@ for (let i = 0; i < our_team_array.length; i++) {
 
     my_div.innerHTML += 
     `
-    <img src="${our_team_array[i].image}" class='card-img-top' alt='...'>
+    <img src="./img/${our_team_array[i].image}" class='card-img-top' alt='...'>
     <div class='card-body'>
         <h5 class='card-title'>${our_team_array[i].first_name} ${our_team_array[i].last_name}</h5>
         <p class='card-text'>${our_team_array[i].role}</p>
