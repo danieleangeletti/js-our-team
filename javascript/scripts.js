822c02bbc9c13335f2bbb4059b6a805f9cc78a3a
danieleangeletti <angelettidaniele@gmail.com>
2023-12-18 14:26:53 +0100

    inizializzazione e milestone 0 (senza function)

---
+++ javascript/scripts.js
@@ -0,0 +1,57 @@
+const our_team_array = [];
+
+const src_img_array = [
+    "./img/wayne-barnett-founder-ceo.jpg",
+    "./img/angela-caroll-chief-editor.jpg",
+    "./img/walter-gordon-office-manager.jpg",
+    "./img/angela-lopez-social-media-manager.jpg",
+    "./img/scott-estrada-developer.jpg",
+    "./img/barbara-ramos-graphic-designer.jpg"
+]
+
+const wayne_barnett = {
+    first_name: "Wayne",
+    last_name: "Barnett",
+    role: "Founder & CEO",
+    image: "./img/wayne-barnett-founder-ceo.jpg"
+}
+
+const angela_caroll = {
+    first_name: "Angela",
+    last_name: "Caroll",
+    role: "Chief Editor",
+    image: "./img/angela-caroll-chief-editor.jpg"
+}
+
+const walter_gordon = {
+    first_name: "Walter",
+    last_name: "Gordon",
+    role: "Office Manager",
+    image: "./img/walter-gordon-office-manager.jpg"
+}
+
+const angela_lopez = {
+    first_name: "Angela",
+    last_name: "Lopez",
+    role: "Social Media Manager",
+    image: "./img/angela-lopez-social-media-manager.jpg"
+}
+
+const scott_estrada = {
+    first_name: "Scott",
+    last_name: "Estrada",
+    role: "Developer",
+    image: "./img/scott-estrada-developer.jpg"
+}
+
+const barbara_ramos = {
+    first_name: "Barbara",
+    last_name: "Ramos",
+    role: "Graphic Designer",
+    image: "./img/barbara-ramos-graphic-designer.jpg"
+}
+
+our_team_array.push(wayne_barnett, angela_caroll, walter_gordon, angela_lopez, scott_estrada, barbara_ramos);
+
+console.log(our_team_array)
+
